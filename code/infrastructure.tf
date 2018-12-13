a80d7f4be1ce9f5a77048fa51863daf7cf35bfe5
Ed Morgan <ed@edmorgan.info>
2018-12-13 14:43:20 +0000

    Cleaning up terraform shite

--- code/infrastructure.tf
+++ code/infrastructure.tf
@@ -85,29 +85,3 @@ resource "vsphere_virtual_machine" "vmug-mysql" {
     template_uuid = "${data.vsphere_virtual_machine.template.id}"
   }
 }
-
-resource "vsphere_virtual_machine" "vmug-elk" {
-  name             = "vmug-elk"
-  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
-  datastore_id     = "${data.vsphere_datastore.datastore.id}"
-
-  num_cpus = 4
-  memory   = 4096
-  guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
-  scsi_type = "${data.vsphere_virtual_machine.template.scsi_type}"
-
-  network_interface {
-    network_id = "${data.vsphere_network.network.id}"
-    //adapter_type = "vmxnet3"
-  }
-
-  disk {
-    label = "disk0"
-    size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
-    eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
-    thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"  
-    }
-  clone {
-    template_uuid = "${data.vsphere_virtual_machine.template.id}"
-  }
-}
